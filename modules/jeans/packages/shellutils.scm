ac2c185e7b303453a0961bab328bdcafbe04479a
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-15 15:59:10 +0800

    fixed.

--- modules/jeans/packages/shellutils.scm
+++ modules/jeans/packages/shellutils.scm
@@ -1,20 +1,53 @@
-(define-module (my-packages starship)
+(define-module (jeans packages shellutils)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix gexp)
+  #:use-module (guix utils)
   #:use-module (guix packages)
+  #:use-module (guix download)
   #:use-module (guix git-download)
   #:use-module (guix build-system cargo)
+  #:use-module (guix build-system copy)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system go)
+  #:use-module ((guix build-system python) #:select (pypi-uri))
+  #:use-module (guix build-system pyproject)
   #:use-module (gnu packages)
-  #:use-module (gnu packages version-control)   ; git-minimal
-  #:use-module (gnu packages cmake)             ; 如果需要
-  #:use-module ((gnu packages crates-io) #:prefix crate:)
-  #:use-module (guix utils)
-  #:use-module (guix gexp))
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages bison)
+  #:use-module (gnu packages check)
+  #:use-module (gnu packages cmake)
+  #:use-module (gnu packages flex)
+  #:use-module (gnu packages golang-build)
+  #:use-module (gnu packages golang-check)
+  #:use-module (gnu packages golang-xyz)
+  #:use-module (gnu packages libunistring)
+  #:use-module (gnu packages ncurses)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages pcre)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-build)
+  #:use-module (gnu packages python-check)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages readline)
+  #:use-module (gnu packages ruby)
+  #:use-module (gnu packages ruby-check)
+  #:use-module (gnu packages ruby-xyz)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages textutils)
+  #:use-module (gnu packages tmux)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages vim))
 
-(define-public starship-git
+
+(define-public starship-latest
   (let ((commit "cc493347dcb1d129df29e87f978443262c3b1730")   ; ← 改成最新
         (revision "0"))
     (package
       (inherit (specification->package "starship"))   ; 或直接写 (@@ (gnu packages rust-apps) starship)
-      (name "starship-git")
+      (name "starship-latest ")
       (version (git-version "1.24.2" revision commit))
       (source
        (origin
@@ -31,3 +64,5 @@
       ;; ... 保留原有的 arguments、inputs、native-inputs 等
       ;; 如果要覆盖某部分，可以在这里写 (arguments #~(...)) 来覆盖
       )))
+
+starship-git
