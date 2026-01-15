5a974f049fe48519d1635e543abb9c9447393f04
BrokenShine <brokenshine@noreply.codeberg.org>
2026-01-15 17:19:57 +0800

    Update jeans.scm

--- modules/jeans.scm
+++ modules/jeans.scm
@@ -1,14 +1,18 @@
-(define-module (jeans))
+;;; SPDX-FileCopyrightText: 2025 Hilton Chain <hako@ultrarare.space>
+;;; SPDX-License-Identifier: GPL-3.0-or-later
+;;;
+;;; Based on Rosenthal by Hilton Chain
+;;; Modified and adapted for the jeans channel by brokenshine <xchai404@gmail.com>
 
-;; Re-export commonly-used modules.
+
+(define-module (jeans))
 
 (eval-when (eval load compile)
   (begin
     (define %public-modules
       '((gnu)
         (guix utils)
-        (jeans packages fonts)
-        (jeans packages shellutils)))
+        (jeans packages fonts)))
 
     (for-each (let ((i (module-public-interface (current-module))))
                 (lambda (m)
