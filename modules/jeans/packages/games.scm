3a0ac5b39409229e35ef7d8e46ad623d1663a164
BrokenShine <brokenshine@noreply.codeberg.org>
2026-03-08 01:12:08 +0800

    bump version.

--- modules/jeans/packages/games.scm
+++ modules/jeans/packages/games.scm
@@ -46,53 +46,14 @@
   #:use-module (guix gexp)
   #:use-module (guix git-download)
   #:use-module (guix packages)
-  #:use-module (guix utils))
+  #:use-module (guix utils)
+  #:use-module (jeans packages hardware))
 
-(define-public opentabletdriver-udev-rules
-  (package
-    (name "opentabletdriver-udev-rules")
-    (version "0.6.6.2")
-    (source
-      (origin
-        (method git-fetch)
-        (uri (git-reference
-               (url "https://github.com/OpenTabletDriver/OpenTabletDriver")
-               (commit (string-append "v" version))))
-        (file-name (git-file-name name version))
-        (sha256
-          (base32 "1r9q1qmhca5q90kwd80cqbajkdx5crsiafywjy5zhq5gswasis1r"))))
-    (build-system gnu-build-system)
-    (arguments
-      (list #:modules '((guix build utils)
-                        (guix build gnu-build-system)
-                        (ice-9 popen)
-                        (ice-9 textual-ports))
-            #:phases
-            #~(modify-phases %standard-phases
-                (delete 'configure)
-                (delete 'check)
-                (replace 'build
-                  (lambda _
-                    (let* ((pipe (open-input-pipe "bash generate-rules.sh"))
-                           (output (get-string-all pipe)))
-                      (close-pipe pipe)
-                      (call-with-output-file "70-opentabletdriver.rules"
-                        (lambda (port)
-                          (put-string port output))))))
-                (replace 'install
-                  (lambda _
-                    (install-file "70-opentabletdriver.rules"
-                                  (string-append #$output "/lib/udev/rules.d")))))))
-    (native-inputs (list bash-minimal jq))
-    (home-page "https://opentabletdriver.net")
-    (synopsis "UDev rules for OpenTabletDriver")
-    (description "Open source, cross-platform, user-mode tablet driver")
-    (license license:lgpl3+)))
 
 (define-public osu-lazer-tachyon-bin
   (package
     (name "osu-lazer-tachyon-bin")
-    (version "2026.304.0-tachyon")
+    (version "2026.307.0-tachyon")
     (source
       (origin
         (method url-fetch)
@@ -101,7 +62,7 @@
                          version
                          "/osu.AppImage"))
         (sha256
-          (base32 "0kyn2vapjp41nankbkd0s6xa431sxsc8ydj0d6kim1xhdv7jpp4j"))))
+          (base32 "08rw1p15l9shm5fydshfjkabp3d1151f3za6bpakaq2sm8n7hh1d"))))
     (build-system copy-build-system)
     (arguments
       (list #:install-plan
