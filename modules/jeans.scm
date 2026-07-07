c9d67c9a946f0d0d72bc1653c018430d8bf9a1c4
ShineBreaker <102544159+ShineBreaker@users.noreply.github.com>
2026-07-07 12:08:16 +0800

    FIX: export jeans packages tools from (jeans)

--- modules/jeans.scm
+++ modules/jeans.scm
@@ -20,6 +20,7 @@
                   (jeans packages rust-crates)
                   (jeans packages theme)
                   (jeans packages nix-ld)
+                 (jeans packages tools)
 
                  (jeans services hardware)
                  (jeans services nix-ld)))
